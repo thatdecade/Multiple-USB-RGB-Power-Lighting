3f06aecb6146d73b7da8c81adf1511348c697ce2
Dustin Westaby <dwestaby@2g-eng.com>
2020-08-25 21:21:55 -0500

    Removed delays

--- arduino_code/arduino_code.ino
+++ arduino_code/arduino_code.ino
@@ -11,7 +11,7 @@
 #endif
 
 #define SPIN_SPEED 60
-#define SPIN_DURATION 10*8
+#define SPIN_DURATION 80
 
 #define SOLID_COLOR_FADE_SPEED 10
 #define BLANKING_OFFSET 4
@@ -30,7 +30,7 @@ Adafruit_NeoPixel pixels = Adafruit_NeoPixel(NUM_LEDS, NEO_PIN);
 #define RED    0xFF0000
 #define BLUE   0x0000FF
 #define GREEN  0x00FF00
-#define TEAL   0x00FFFF
+#define CYAN   0x00FFFF
 #define YELLOW 0xFF00FF
 
 //set starup colors
@@ -38,6 +38,16 @@ uint32_t color1  = BLUE;
 uint32_t color2  = BLUE;
 
 uint32_t prevTime;
+
+typedef enum
+{
+  START = 0,
+  SPINNING,
+  FADING,
+  IDLE_MODE,
+} mode_e;
+
+mode_e system_mode = START;
 
 void setup() 
 {
@@ -52,23 +62,39 @@ void setup()
   pinMode(GREEN_PIN, INPUT);
   
   prevTime = millis();
+
+  system_mode = START;
 }
 
 void loop() {
-  static uint32_t previous_color1 = 0;
-  static uint32_t previous_color2 = 0;
 
   check_for_input_changes();
 
+  update_state_machine();
+
+  draw_animation();
+}
+
+void  update_state_machine()
+{
+  static uint32_t previous_color1 = 0;
+  static uint32_t previous_color2 = 0;
+  
+  //reset animations
   if( (color1 != previous_color1) ||
       (color2 != previous_color2))
   {
     previous_color1 = color1;
     previous_color2 = color2;
-    
+
+    system_mode = SPINNING;
+  }
+}
+
+void draw_animation()
+{
     animate_spin();
     solid_color_fade_down();
-  }
 }
 
 void check_for_input_changes()
@@ -126,41 +152,26 @@ void check_for_input_changes()
   
 }
 
-void solid_color_fade_down()
-{
-  uint8_t  i,j;
-  
-  //set to solid color, while also dimming gradually.
-  for(i=0; i<16; i++) 
-  {
-    for(j=1;j<16;j++)
-    {
-      pixels.setBrightness(map(i*15+j,0,254,254,45));
-      pixels.show();
-      delay(SOLID_COLOR_FADE_SPEED);
-    }
-    pixels.setPixelColor(           i, color1);
-    pixels.setPixelColor(NUM_LEDS-1-i, color2);
-
-    if((i+BLANKING_OFFSET)<16)
-    {
-      pixels.setPixelColor(           (i+BLANKING_OFFSET), 0);
-      pixels.setPixelColor(NUM_LEDS-1-(i+BLANKING_OFFSET), 0);
-    }
-
-    
-    pixels.show();
-    delay(SOLID_COLOR_FADE_SPEED);
-  }
-}
 void animate_spin()
 {
-  uint8_t  i,j;
-  int offset = 0;
-  
-  // Spinny wheels (4 LEDs on at a time)
-  for(j=0;j<SPIN_DURATION;j++)
+  static uint8_t i = 0;
+  static uint8_t j = 0;
+  static int offset = 0;
+  static mode_e previous_system_mode = START;
+
+  if(previous_system_mode != system_mode)
   {
+    i = 0;
+    j = 0;
+    previous_system_mode = system_mode;
+  }
+
+  if((system_mode == SPINNING) && ((prevTime + SPIN_SPEED) < millis()))
+  {
+    prevTime = millis();
+    
+    // Spinny wheels (4 LEDs on at a time)
+  
     for(i=0; i<NUM_LEDS_ONE_WHEEL; i++) 
     {
       uint32_t c = 0;
@@ -175,8 +186,60 @@ void animate_spin()
     }
     offset++;
     pixels.show();
-    delay(SPIN_SPEED);
+
+    j++;
+    if(j >= SPIN_DURATION)
+    {
+      system_mode = FADING;
+      previous_system_mode = system_mode;
+    }
+  }
+}
+
+void solid_color_fade_down()
+{
+  static uint8_t i = 0;
+  static uint8_t j = 0;
+  static mode_e previous_system_mode = START;
+  
+  if(previous_system_mode != system_mode)
+  {
+    i = 0;
+    j = 0;
+    previous_system_mode = system_mode;
   }
 
-  prevTime = millis();
+  if((system_mode == FADING) && ((prevTime + SOLID_COLOR_FADE_SPEED) < millis()))
+  {
+    prevTime = millis();
+    
+    //set to solid color, while also dimming gradually.
+
+    pixels.setBrightness(map(i*15+j,0,254,254,45));
+
+    j++;
+    if(j >= 16)
+    {
+      j = 0;
+      pixels.setPixelColor(           i, color1);
+      pixels.setPixelColor(NUM_LEDS-1-i, color2);
+  
+      if((i+BLANKING_OFFSET)<16)
+      {
+        pixels.setPixelColor(           (i+BLANKING_OFFSET), 0);
+        pixels.setPixelColor(NUM_LEDS-1-(i+BLANKING_OFFSET), 0);
+      }
+  
+  
+      i++;
+      if(i >= 16)
+      {
+        system_mode = IDLE_MODE;
+        previous_system_mode = system_mode;
+      }
+    }
+    
+    pixels.show();
+  }
 }
+
